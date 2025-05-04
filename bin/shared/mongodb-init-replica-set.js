cac19a3a67759877c9e6d432c260f0dd984071cb
IamKaleb21 <hionta16@gmail.com>
2025-05-03 19:28:30 -0500

    first commit

---
+++ bin/shared/mongodb-init-replica-set.js
@@ -0,0 +1,12 @@
+// mongodb-init-replica-set.js
+var replicaSetName = 'overleaf';
+var memberName = 'mongo:27017'; // Asegúrate que coincide con el nombre del servicio mongo
+
+var config = {
+  _id: replicaSetName,
+  members: [
+    { _id: 0, host: memberName }
+  ]
+};
+
+rs.initiate(config);
